86117c6d0cd54f696494cae2e5f1b4349a57a86e
Arik Sosman <git@arik.io>
2019-12-10 16:21:09 -0800

    create integration test scaffold

---
+++ test/web_client.ts
@@ -0,0 +1,11 @@
+import * as chai from 'chai';
+
+const assert = chai.assert;
+
+describe('Web Client Integration Test', () => {
+
+	it('should complete a full setup round with the server', () => {
+		assert.equal(2 + 2, 4);
+	});
+
+});
